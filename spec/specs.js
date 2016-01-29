b481062905d0edc0d5686cae4cc5fe1986ee1e9c
Molly Curtin <patternandshape@gmail.com>
2016-01-29 15:41:02 -0800

    add cleaned up css and fix indentation

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
-describe("Pizza", function(){
-  it("will take in user's choices to return all pizza properties", function(){
+describe("Pizza", function() {
+  it("will take in user's choices to return all pizza properties", function() {
     var testPizza = new Pizza("Small", "Veggissimo", "olives", "tempeh");
     expect(testPizza.pizzaSize).to.equal("Small");
     expect(testPizza.pizzaStyle).to.equal("Veggissimo");
@@ -8,8 +8,8 @@ describe("Pizza", function(){
     expect(testPizza.pizzaPrice).to.eql([]);
   });
 
-  it("will return the price for the pizza based on selected items", function(){
-    var testPizza = new Pizza("Large","Veganississimo","olives","tempeh");
+  it("will return the price for the pizza based on selected items", function() {
+    var testPizza = new Pizza("Large", "Veganississimo", "olives", "tempeh");
     expect(testPizza.price()).to.eq(30);
   });
 });
